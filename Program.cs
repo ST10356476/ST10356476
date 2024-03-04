bf4f62d070e9ab99db40832f0380d2444fb07518
Delron Munyai <126163646+ST10356476@users.noreply.github.com>
2024-03-04 10:04:45 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("I am Munyai Delron");
+Console.WriteLine("The Installation of dotnet was successful");
